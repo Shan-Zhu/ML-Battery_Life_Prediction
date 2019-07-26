cd4d5a2fc4628bca75df9fc01cd1c305898ca2b9
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2019-07-26 11:52:42 +0800

    Update ML-models.py

--- ML-models.py
+++ ML-models.py
@@ -20,7 +20,7 @@ from sklearn import tree
 import os
 
 # input data
-data_train=pd.read_csv('C:/Users/Administrator/Desktop/battery-life/data-2-1-2.csv',sep=',')
+data_train=pd.read_csv('data-2-1.csv',sep=',') #or 'data-100-1.csv'
 
 labels=data_train['Type'][:,np.newaxis]
 features=data_train.drop('Type', axis=1)
